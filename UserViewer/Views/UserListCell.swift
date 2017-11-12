d79fe0a22e791cafacab888c14350d3bd52658af
sophie <sophie.ettl@gmail.com>
2017-11-12 21:34:20 +0100

    added cell subclass for user list

--- UserViewer/Views/UserListCell.swift
+++ UserViewer/Views/UserListCell.swift
@@ -16,36 +16,40 @@ extension UITableViewCell {
 }
 
 class UserListCell: UITableViewCell {
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-        self.detailTextLabel?.textColor = .gray
-        self.imageView?.contentMode = .scaleAspectFill
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var registrationDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        registrationDateLabel.textColor = .lightGray
+        avatarImageView.contentMode = .scaleAspectFill
+        avatarImageView.clipsToBounds = true
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
+    override func layoutSubviews() {
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.size.width / 2
+        super.layoutSubviews()
     }
     
     func configure(user: User) {
-        textLabel?.text = user.name
+        nameLabel.text = user.name
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         let date = ISO8601DateFormatter().date(from: user.registrationDate)
         let s = dateFormatter.string(from: date!)
         
-        detailTextLabel?.text = "member since: \(s)"
+        registrationDateLabel.text = "member since: \(s)"
         guard let imageURL = URL(string: user.avatarURL) else {
             return
         }
-        imageView?.af_setImage(withURL: imageURL,
-                               placeholderImage: #imageLiteral(resourceName: "placeholder"),
-                               imageTransition: .crossDissolve(0.2))
+        avatarImageView.af_setImage(withURL: imageURL,
+                                    placeholderImage: #imageLiteral(resourceName: "placeholder"),
+                                    imageTransition: .crossDissolve(0.2))
     }
     
     override func prepareForReuse() {
         imageView?.image = nil
     }
 }
-
